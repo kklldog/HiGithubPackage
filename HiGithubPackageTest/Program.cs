7d1aae0888e8a2e72b367b7b86e22b64ccf7f4b1
kklldog <kklldog@qq.com>
2020-03-19 01:30:29 +0800

    update

---
+++ HiGithubPackageTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HiGithubPackageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HiGithubPackage.GithubPackage.Hi();
+
+            Console.ReadLine();
+        }
+    }
+}
