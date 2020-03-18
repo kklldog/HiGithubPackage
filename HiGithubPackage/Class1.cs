c2b9b485bda03835a2020425c9f5b03e59bc2cf1
kklldog <kklldog@qq.com>
2020-03-19 00:40:28 +0800

    添加项目文件。

---
+++ HiGithubPackage/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HiGithubPackage
+{
+    public class GithubPackage
+    {
+        public static void Hi ()
+        {
+            Console.WriteLine("Hi GithubPackage ~");
+        }
+    }
+}
